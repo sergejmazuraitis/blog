385ccdc8840135637858288d07778d0425043adf
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-04-14 11:03:16 +0300

    Start "blog" project

---
+++ src/main/java/lt/codeacademy/project/blog/model/Post.java
@@ -0,0 +1,26 @@
+package lt.codeacademy.project.blog.model;
+
+import lombok.Data;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "Post")
+public class Post {
+
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "VARCHAR(36)", updatable = false)
+    @Type(type = "uuid-char")
+    private UUID id;
+
+    private String name;
+
+    private String description;
+
+    private String content;
+
+}
