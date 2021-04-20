d6ec32ad1cc521f027c1c7209a4eae50cb81bb70
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-04-20 13:05:48 +0300

    create Comment service

---
+++ src/main/java/lt/codeacademy/project/blog/service/CommentService.java
@@ -0,0 +1,20 @@
+package lt.codeacademy.project.blog.service;
+
+import lt.codeacademy.project.blog.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
+public interface CommentService {
+    void addComment(Comment comment);
+
+    Comment getCommentById(UUID id);
+
+    Page<Comment> getAllCommentsWithPages(Pageable pageable);
+
+    void updateComment(Comment comment);
+
+    void deleteComment(UUID id);
+
+}
---
+++ src/main/java/lt/codeacademy/project/blog/service/CommentServiceImpl.java
@@ -0,0 +1,42 @@
+package lt.codeacademy.project.blog.service;
+
+import lt.codeacademy.project.blog.exception.CommentFoundException;
+import lt.codeacademy.project.blog.model.Comment;
+import lt.codeacademy.project.blog.repository.CommentRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
+public class CommentServiceImpl implements CommentService {
+    private final CommentRepository commentRepository;
+
+    public CommentServiceImpl(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Override
+    public void addComment(Comment comment) {
+        commentRepository.save(comment);
+    }
+
+    @Override
+    public Comment getCommentById(UUID id) {
+        return commentRepository.findById(id).orElseThrow(CommentFoundException::new);
+    }
+
+    @Override
+    public Page<Comment> getAllCommentsWithPages(Pageable pageable) {
+        return commentRepository.findAll(pageable);
+    }
+
+    @Override
+    public void updateComment(Comment comment) {
+        commentRepository.save(comment);
+    }
+
+    @Override
+    public void deleteComment(UUID id) {
+        commentRepository.deleteById(id);
+    }
+}
