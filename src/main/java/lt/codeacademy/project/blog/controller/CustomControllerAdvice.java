c892968c237c032a2e0944bf4254cd6168969020
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-04-25 20:32:51 +0300

    create userNotFound template and add ExceptionHandler for UserFoundException

--- src/main/java/lt/codeacademy/project/blog/controller/CustomControllerAdvice.java
+++ src/main/java/lt/codeacademy/project/blog/controller/CustomControllerAdvice.java
@@ -2,6 +2,7 @@ package lt.codeacademy.project.blog.controller;
 
 import lt.codeacademy.project.blog.exception.BlogPostFoundException;
 import lt.codeacademy.project.blog.exception.CommentFoundException;
+import lt.codeacademy.project.blog.exception.UserFoundException;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
@@ -27,6 +28,11 @@ public class CustomControllerAdvice {
     @ExceptionHandler(CommentFoundException.class)
     public String commentNotFound() {
         return "commentNotFound";
+    }
+
+    @ExceptionHandler(UserFoundException.class)
+    public String userNotFound(){
+        return "userNotFound";
     }
 
 }
